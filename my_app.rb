d8bffa1d2f4a6b7a202f8b1bcf0d6c3bcb201214
Galina <yermichevagalina@gmail.com>
2013-11-27 15:20:49 -0800

    added yaml meta data to posts

--- my_app.rb
+++ my_app.rb
@@ -1,11 +1,35 @@
 require 'sinatra'
+require 'yaml'
 
 class MyApp < Sinatra::Base
-   get "/" do
+  before do 
+    @posts = Dir.glob("views/posts/*.erb").map do |post_name|
+      post_name.split("/").last.slice(0..-5)
+    end
+  end
+
+  get "/" do
     erb :index
   end
 
   get "/blog/:post_name" do
-   erb "posts/#{params[:post_name]}".to_sym
+    html = erb("posts/#{params[:post_name]}".to_sym, layout: false)
+    html = html.split("\n\n", 2).last
+    erb html
   end
+
+  def meta_data 
+    if @meta_data 
+      @meta_data
+    else
+       @meta_data = {}
+      @posts.each do |post|
+        html = erb("posts/#{post}".to_sym, layout: false)
+        meta = YAML.load(html.split("\n\n", 2).first)
+        @meta_data[post] = meta
+      end
+      @meta_data
+    end
+  end
+   
 end
